b9c9e7aa38821bd0b2aea3fd274bbc1529c5bfaf
arulpks-git <42057933+arulpks-git@users.noreply.github.com>
2020-01-18 19:02:33 +0530

    Add files via upload

---
+++ precache-manifest.3c8a78bf42f1a2ba7513fc3b20cb369f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39bce837048e5a6c95e",
+    "url": "/static/css/main.7c496f5c.chunk.css"
+  },
+  {
+    "revision": "a39bce837048e5a6c95e",
+    "url": "/static/js/main.a39bce83.chunk.js"
+  },
+  {
+    "revision": "cae24b44bca2e7fb4887",
+    "url": "/static/css/1.3bb16591.chunk.css"
+  },
+  {
+    "revision": "cae24b44bca2e7fb4887",
+    "url": "/static/js/1.cae24b44.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a5641299916f3397ad7009d729da6f3",
+    "url": "/index.html"
+  }
+];
