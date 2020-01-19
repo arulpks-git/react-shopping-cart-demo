89c599388be280f9167ce5f85bb8a0593fb904e4
Arulprakash <arulprakash@Arulprakashs-MacBook-Pro.local>
2020-01-19 07:51:45 +0530

    Updates

--- precache-manifest.b0270a9908f5df2d4002a3cb160fdc2c.js
+++ precache-manifest.b0270a9908f5df2d4002a3cb160fdc2c.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "db20f5c1bab4a8dbce00",
+    "revision": "46b704ae7c333e76a970",
     "url": "/react-shopping-cart-demo/static/css/main.be5def69.chunk.css"
   },
   {
-    "revision": "db20f5c1bab4a8dbce00",
-    "url": "/react-shopping-cart-demo/static/js/main.db20f5c1.chunk.js"
+    "revision": "46b704ae7c333e76a970",
+    "url": "/react-shopping-cart-demo/static/js/main.46b704ae.chunk.js"
   },
   {
-    "revision": "43ea1f4c72d57dbd0ec6",
+    "revision": "92ad71fdcfee6c6fbd4f",
     "url": "/react-shopping-cart-demo/static/css/1.c88ae462.chunk.css"
   },
   {
-    "revision": "43ea1f4c72d57dbd0ec6",
-    "url": "/react-shopping-cart-demo/static/js/1.43ea1f4c.chunk.js"
+    "revision": "92ad71fdcfee6c6fbd4f",
+    "url": "/react-shopping-cart-demo/static/js/1.92ad71fd.chunk.js"
   },
   {
     "revision": "453f9ad920725182139f",
     "url": "/react-shopping-cart-demo/static/js/runtime~main.453f9ad9.js"
   },
   {
-    "revision": "267e76c69e3b37a49045a13dc8c015ec",
+    "revision": "38c5947ba5b5d172ae6cba001e674cb9",
     "url": "/react-shopping-cart-demo/index.html"
   }
 ];
